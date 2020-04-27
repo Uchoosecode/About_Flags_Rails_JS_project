e5455b09a70c730ee311e1e1546bbfbf9aece00f
Uchoosecode <awittyl44@gmail.com>
2020-04-27 09:14:30 -0400

    change radio to checkbox buttons, add backgroud

--- frontend/javascripts/color.js
+++ frontend/javascripts/color.js
@@ -3,7 +3,6 @@ class Color {
         this.id = data.id;
         this.name = data.name;
     }
-
 }
 
     color_events = []
@@ -19,81 +18,50 @@ class Color {
                 let color = new Color(data);
                 let div = colorBtn;
                 let label = document.createElement("label");
-                let radio = document.createElement("input");
+                let checkbox = document.createElement("input");
                 label.setAttribute('for', `color-${color.name}`);
-                // debugger;
+                
                 label.innerText = color.name;
-                radio.id = `${color.name}`;
-                radio.type = "radio";
-                radio.className = "fillStyle";
-                radio.onclick = function colorChange() {
-                    color_events.push(event.toElement);
-                    // debugger;
-        if(shape_events[shape_events.length-1].innerText == "SQUARE") {
-            ctx.restore();
-            ctx.fillStyle = color_events[color_events.length-1].value;
-            ctx.fillRect(0, 0, WIDTH/2, HEIGHT);
-        }
-        else if (shape_events[shape_events.length-1].innerText == "CIRCLE") {
-            ctx.restore();
-            ctx.fillStyle = color_events[color_events.length-1].value;
-            ctx.beginPath();
-            ctx.arc(WIDTH/2, HEIGHT/2, 125, 0, Math.PI * 2);
-            ctx.fill();
-        }
-        else if (shape_events[shape_events.length-1].innerText == "TRIANGLE"){
-            ctx.restore();
-            ctx.fillStyle = color_events[color_events.length-1].value;
-            ctx.beginPath();
-            ctx.moveTo(250, 125);
-            ctx.lineTo(375, 250);
-            ctx.lineTo(125, 250);
-            ctx.fill();
-        }
-        else {
-           message = alert("You need to my a shape first");
-        }
-    };
-                radio.name = "color";
-                radio.value = color.name;
+                checkbox.id = `${color.id}`;
+                checkbox.type = "checkbox";
+                checkbox.className = "fillStyle";
+                checkbox.onclick = function colorChange() {
+                color_events.push(event.toElement);
+                   
+                if(shape_events[shape_events.length-1].innerText == "SQUARE") {
+                    ctx.restore();
+                    ctx.fillStyle = color_events[color_events.length-1].value;
+                    ctx.fillRect(0, 0, WIDTH/2, HEIGHT);
+                }
+                else if (shape_events[shape_events.length-1].innerText == "CIRCLE") {
+                    ctx.restore();
+                    ctx.fillStyle = color_events[color_events.length-1].value;
+                    ctx.beginPath();
+                    ctx.arc(WIDTH/2, HEIGHT/2, 125, 0, Math.PI * 2);
+                    ctx.fill();
+                }
+                else if (shape_events[shape_events.length-1].innerText == "TRIANGLE"){
+                    ctx.restore();
+                    ctx.fillStyle = color_events[color_events.length-1].value;
+                    ctx.beginPath();
+                    ctx.moveTo(250, 125);
+                    ctx.lineTo(375, 250);
+                    ctx.lineTo(125, 250);
+                    ctx.fill();
+                }
+                else if (shape_events[shape_events.length-1].innerText == "BACKGROUND") {
+                    ctx.restore();
+                    ctx.fillStyle = color_events[color_events.length-1].value;
+                    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+                }
+                else {
+                message = alert("You need to my a shape first");
+                }
+            };
+                checkbox.name = "color";
+                checkbox.value = color.name;
                 div.appendChild(label);
-                div.appendChild(radio);
+                div.appendChild(checkbox);
             });
         });
     };
-
-    // function colorChange() {
-    //     if(shape_events[shape_events.length-1].innerText == "SQUARE") {
-    //         ctx.restore();
-    //         ctx.fillStyle = "red";
-    //         debugger;
-    //         ctx.fillRect(0, 0, WIDTH/2, HEIGHT);
-    //     }
-    //     else if (shape_events[shape_events.length-1].innerText == "CIRCLE") {
-    //         ctx.restore();
-    //         ctx.fillStyle = "gold";
-    //         ctx.beginPath();
-    //         ctx.arc(WIDTH/2, HEIGHT/2, 125, 0, Math.PI * 2);
-    //         ctx.fill();
-    //     }
-    //     else if (shape_events[shape_events.length-1].innerText == "TRIANGLE"){
-    //         ctx.restore();
-    //         ctx.fillStyle = "blue";
-    //         ctx.beginPath();
-    //         ctx.moveTo(250, 125);
-    //         ctx.lineTo(375, 250);
-    //         ctx.lineTo(125, 250);
-    //         ctx.fill();
-    //     }
-    //     else {
-    //        message = alert("You need to my a shape first");
-    //     }
-    // };
-    
-colorBtn.addEventListener("click", function () {
-     let grabbed = document.getElementsByClassName("fillStyle")
-
-    console.log(grabbed)
-});
-
-
