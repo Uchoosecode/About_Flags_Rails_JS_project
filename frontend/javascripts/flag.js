c1d0b40315c026ffa39de5d27c48ed178473b1e1
Uchoosecode <awittyl44@gmail.com>
2020-04-27 09:12:59 -0400

    adjusted the last GET request

--- frontend/javascripts/flag.js
+++ frontend/javascripts/flag.js
@@ -1,5 +1,5 @@
 class Flag {
-    static = []
+    static all = [];
 
      constructor(data) {
          this.id = data.id
@@ -11,7 +11,7 @@ class Flag {
      }
 
      save() {
-       Flag.all.push(this)
+       Flag.all.push(this);
      }
 }
 document.querySelector("form").addEventListener("submit", createFlag);
@@ -22,43 +22,60 @@ async function createFlag(e) {
     const name = document.getElementById('name').value;
     const flagDescrp= document.getElementById('description').value;
     const yearCreated = document.getElementById('year_created').value;
-    const slctColor = document.querySelectorAll("option")
+    const slctColor = document.querySelectorAll("input")
     const color_ids = [];
     
     for (let i = 0; i < slctColor.length; i++) {
-      if (slctColor[i].selected) {
-        color_ids.push(slctColor[i].value);
+
+      if (slctColor[i].checked) {
+        color_ids.push(slctColor[i].id);
       }
     }
+
+    const flagURI = canvas.toDataURL();
+    imgConverted.src = flagURI;
   
     const strongParams = {
       flag: {
         name,
         description: flagDescrp,
         year_created: yearCreated,
-        color_ids
+        img: flagURI,
+        color: color_ids
       }
     }
-  debugger;
     sendHTTPRequest('POST', 'http://localhost:3000/api/v1/flags', strongParams);
   
     showFlags();
+
+    clearCanvas();
+    
   }
 
   function showFlags() {
-
+    Flag.all = []; 
+    let flagList = document.querySelector("flag-list");
+    
     sendHTTPRequest('GET', 'http://localhost:3000/api/v1/flags').then(flagData => {
-       
-        flagData.forEach(data => {
+      Flag.all = flagData.map(flag => new Flag(flag));
 
-            flag = new Flag(data);
-            let showFlags = document.getElementById("flag-list");
-            let div = document.createElement("div");
-            div.value = data.name;
-            div.innerText = data.name;
-            
-            showFlags.appendChild(div);
-            console.log(div)
+        Flag.all.forEach(data => {
+          let div = document.createElement("div");
+          let h3 = document.createElement("h3");
+          let h2 = document.createElement("h2");
+          
+          h2.textContent = data.name;
+          h3.textContent = data.description;
+          h3.textContext + data.year_created;
+          
+          div.appendChild("h3");
+          div.appendChild("h2");
+          flagList.appendChild("div");
         });
     });
-};
+  };
+
+  function clearCanvas() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    drawCanvas();
+  };
