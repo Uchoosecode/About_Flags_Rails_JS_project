279aa03cb127b047e2683d06d8ada404221cc4bf
Uchoosecode <awittyl44@gmail.com>
2020-04-30 03:58:03 -0400

    active storge validation

--- flags-backend/app/models/flag.rb
+++ flags-backend/app/models/flag.rb
@@ -1,4 +1,8 @@
 class Flag < ApplicationRecord
   has_many :flag_colors
   has_many :colors, through: :flag_colors
+
+  has_many_attached :images
+
+  validates :name, presence: true, uniqueness: true
 end
