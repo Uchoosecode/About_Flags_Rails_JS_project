053a24c6c15803b061951b4d70e004ba9f1fb591
Uchoosecode <awittyl44@gmail.com>
2020-05-01 08:23:46 -0400

    changed from has many to has one image

--- flags-backend/app/models/flag.rb
+++ flags-backend/app/models/flag.rb
@@ -2,7 +2,7 @@ class Flag < ApplicationRecord
   has_many :flag_colors
   has_many :colors, through: :flag_colors
 
-  has_many_attached :images
+  has_one_attached :image
 
   validates :name, presence: true, uniqueness: true
 end
